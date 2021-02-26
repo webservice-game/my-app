a42d387766592c1f5f07279bbb185feb4ab2469f
unknown <gopimacha99@gmail.com>
2021-02-26 00:27:53 -0500

    ci

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,13 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+// import App from './App';
+import About from './About';
 import { configure ,shallow} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() })
 describe("counter testing", () =>{
   test('render the title', () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.find('h1').text()).toContain("Welcome to the matchmaking application");
+    const wrapper = shallow(<About/>);
+    expect(wrapper.find('h1').text()).toContain("About Us");
   // const{getByText} =  render(<App />);
   //   const linkElement = getByText('Welcome to the matchmaking application');
   //   expect(linkElement).toBeInTheDocument();
