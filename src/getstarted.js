4807cccbbacf556aa36ba8db36657e5f4d0455ef
Phuong Nguyen <57697430+phuongnth151@users.noreply.github.com>
2021-03-16 13:30:57 -0400

    update phuong branch

--- src/getstarted.js
+++ src/getstarted.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
@@ -14,4 +15,195 @@ const Started = props => {
     )
 
 };
+=======
+import React,{Component, useState, useEffect} from "react";
+import './getstarted.css';
+import Profile from './profile.js';
+import LogoTitle from './LogoTitle.js';
+import Footer from './Footer.js';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import { Container, Row, Col, Image } from 'react-bootstrap';
+import { Paginator } from 'primereact/paginator';
+import { Button } from 'primereact/button';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  withRouter
+} from "react-router-dom";
+import { useHistory } from "react-router-dom";
+
+  
+function Started() { 
+  const [call,setCall]=useState([])
+  const [clicked,setClicked]=useState(false)
+  const [count,setCount]=useState(1)
+  const [friend,setFriend]=useState([])
+  const history = useHistory();
+  let data=[]
+  let or=[]
+  // let count=1;
+  // state= {
+  //   callofduty:[],
+  //   started:false
+
+  // }
+    // constructor(props) {
+    //     super(props);
+
+        // const [products, setProducts] = useState([]);
+      
+        // this.getFoootball = this.getFoootball.bind(this)
+      // }
+      function callOfDuty()  {
+        setClicked(true)
+        let counts = 1
+        setCount(count + 1)
+        var axios = require("axios").default;
+        const options = {
+          method: 'GET',
+          url: `https://call-of-duty-modern-warfare.p.rapidapi.com/leaderboard/${count}/battle`,
+          headers: {
+            'x-rapidapi-key': '2999e842eamshcede1a6258d4821p15ac5cjsnb3ab053a26fc',
+            'x-rapidapi-host': 'call-of-duty-modern-warfare.p.rapidapi.com'
+          }
+        };
+        var self = this;
+        axios.request(options).then(function (response) {
+          console.log(response.data.entries);
+          response.data.entries.map(ele=>{
+              data.push({
+                        rank:ele.rank,
+                        playerName:ele.username,
+                        level:ele.values.level,
+                        name:'Add Friend',
+                        timeplayed:ele.values.timePlayed,
+                        wins:ele.values.wins
+    
+                    })
+                   
+                })
+                // let numbersCopy = data.slice();
+                // console.log(numbersCopy)
+                // setCall([...call, data])
+                setCall(old => [...old, ...data]);
+                // setCall(data);
+                console.log(call)
+                // console.log(data)
+                // self.setState({
+                //   callofduty: data 
+                // })
+                // console.log(this.state.callofduty)
+        }).catch(function (error) {
+          console.error(error);
+        });
+       
+        
+        // axios.request(options).then(function (response) {
+        //     console.log(response.data.api.players);
+        //     response.data.api.players.map(ele=>{
+        //   data.push({
+        //             age:ele.age,
+        //             playerName:ele.player_name,
+        //             position:ele.position
+
+        //         })
+        //     })
+        //     console.log( data)
+        // }).catch(function (error) {
+        //     console.error(error);
+        // });
+      
+      // console.log(this.state.callofduty)
+      }
+       function dateTemplate(rowData, column) {
+        return <div>
+            <a  onClick={(event) => rowColumnClick(rowData)} >{rowData.playerName}</a>
+        </div>;
+    }
+    function addButton(rowData){
+      // console.log(rowData)
+      return <div>
+        <button onClick={(event) => buttonClick(rowData)}>{rowData.name}</button>
+        <button onClick={(event) => removeClick(rowData)}>Remove Friend</button>
+      </div>
+    }
+   function buttonClick(rowdata){
+// console.log(rowdata)
+let array=[rowdata]
+setFriend(old => [...old, ...array]);
+console.log(friend)
+
+   }
+   function removeClick(rowdata){
+     console.log(rowdata)
+   }
+  function rowColumnClick(rowData){
+    console.log(rowData)
+
+    history.push(
+      {
+          pathname:"/Profile",
+          state:{detail:rowData}
+  });
+    // this.props.history.push('/Profile');
+    //     console.log(rowData);
+    //     this.setState({
+    //       started:true
+    //     })
+   }
+  
+   function showmoredata(){
+
+    callOfDuty();
+   }
+    const paginatorLeft = <Button type="button" icon="pi pi-refresh" className="p-button-text" />;
+    const paginatorRight = <Button type="button" icon="pi pi-cloud" className="p-button-text" />;
+    return (
+      <div>
+        <Container fluid className="get-started-con">
+          <LogoTitle />
+          <Row className="get-started-row">
+            <Col className="game-name-board">
+              <div className="game-name-div">
+                <button value="Change value" onClick={callOfDuty} className="game-name-button" style={{color: 'white'}}>Call of Duty </button>
+              </div>
+            </Col>
+            <Col className="game-data-board">
+                <div className="card">
+                {/* {!this.state.started &&  */}
+                  <DataTable value={call} columnResizeMode="fit" className="data-table">
+                  
+                    <Column field="playerName" header="Name" body={dateTemplate} filter filterPlaceholder="Search by name..." className="column-name"></Column>
+                    <Column field="rank" header="Rank"  filter filterPlaceholder="Search by rank..." className="column-rank"></Column>
+                    <Column field="level" header="Level" filter filterPlaceholder="Search by level..." className="column-level"></Column>
+                    <Column field="timeplayed" header="Time-Played" filter filterPlaceholder="Search by time..." className="column-timeplay"></Column>
+                    <Column field="wins" header="Wins" filter filterPlaceholder="Search by wins..." className="column-wins"></Column>
+                    <Column field="name" header="Actions" body={addButton} className="column-actions"></Column>
+                      
+                      {/* <Column field="quantity" header="Quantity"></Column> */}
+                  </DataTable>
+                  <div onClick={showmoredata} className="show-more-button">Show More</div>
+                {/* <Paginator first={1} rows={4} ></Paginator> */}
+                {/* } */}
+                </div>
+            </Col>
+          </Row>
+          <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /> 
+          <Footer />
+        </Container>
+        {/* <Route exact path="/Profile">
+              <Profile/>
+            </Route> */}
+      </div>
+      
+      
+    ) 
+  
+} 
+  
+>>>>>>> Stashed changes
 export default Started;
--- src/home.js
+++ src/home.js
@@ -38,11 +38,17 @@ function Home(){
     return(
         <div className="home" style={{height: "990px", paddingBottom: "100px"}}>
             <div style={{transform:'translatey(300px)'}}>
+<<<<<<< Updated upstream
                 <h1>The Ultimate Destination</h1>
                 <h1>For Gamers Match Making</h1>
                 <button onClick={handleClick}>Get Started</button>
+=======
+        <div className='Line1'> <h1>The Ultimate Destination</h1></div>
+<div className= 'Line2'> <h1>For Gamers Match Making</h1></div>
+               <div className='getStartedButton'>  <button style={{"color":"#000"}} onClick={handleClick}>Get Started</button></div>
+>>>>>>> Stashed changes
             </div>
-            <video autoPlay loop muted
+            {<video autoPlay loop muted
             style={{
                 position:"absolute",
                 height:"100%",
@@ -55,7 +61,7 @@ function Home(){
             }}
             >
                 <source src ={powerslap} type="video/mp4" />
-            </video>
+            </video> }
         </div>        
     )
 }
